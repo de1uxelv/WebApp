f5b3b6164571cfd3656aef1aeec6cc51880ad2ed
Dainis Kuznecovs <danyaa1997@gmail.com>
2020-12-21 15:57:54 +0200

    Add project files.

---
+++ WebApp/Data/Mocks/MockWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Data.Interfaces;
+using WebApp.Models;
+
+namespace WebApp.Data.Mocks
+{
+    public class MockWorker : Interfaces.INewWorker
+    {
+
+
+        public IEnumerable<Interfaces.INewWorker> newWorker => new List<Interfaces.INewWorker> { };
+
+    }
+}
